7474a64bed003c3d967616e4b354cf8ff3accc3d
Roland Sanford <ras9841@rit.edu>
2017-10-29 11:35:23 -0700

    Card-specific styling based on orientation.

--- src/ImageSection.js
+++ src/ImageSection.js
@@ -1,28 +1,32 @@
 import React from 'react';
 import {Card, CardMedia, CardTitle} from 'material-ui/Card';
 
-const pStyle = {
+const pImgStyle = {
   maxHeight: "650px",
   maxWidth: "500px",
   minWidth: "0px"
 };
 
-const lStyle = {
-  marginTop: "8%",
-  marginBottom: "20%"
+
+const lCard = {
+  marginTop: "10%"
+};
+
+const pCard = {
+  marginLeft: "10%",
+  maxWidth: pImgStyle.maxWidth
 }
 
 const ImageSection = (props) => {
   console.log(props.selectedIndex)
   let selectedImage = props.data[props.selectedIndex].image;
   return (
-    <Card>
+    <Card style={selectedImage.orientation === "p" ? pCard : lCard}>
       <CardMedia
         overlay={<CardTitle title={selectedImage.caption} subtitle={selectedImage.subcaption} />}
       >
         <img
           src={selectedImage.src} alt={selectedImage.subcaption}
-          style={selectedImage.orientation === "p" ? pStyle : lStyle}
         />
       </CardMedia>
     </Card>
